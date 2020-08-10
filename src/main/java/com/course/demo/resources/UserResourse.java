dda26e0a23dc71bb8c05f91ef4418fd2c95aa278
Andre Faria <andrefaria-1@hotmail.com>
2020-08-10 12:22:54 -0300

    User insertion with POST

--- src/main/java/com/course/demo/resources/UserResourse.java
+++ src/main/java/com/course/demo/resources/UserResourse.java
@@ -1,5 +1,6 @@
 package com.course.demo.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -10,9 +11,11 @@ import com.course.demo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -36,5 +39,16 @@ public class UserResourse {
         return ResponseEntity.ok().body(new UserDTO(obj));
 
     }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> insert(@RequestBody UserDTO objDTO){
+        User obj = service.fromDTO(objDTO); // converts UserDTO to User
+        obj = service.insert(obj);
+        // uri creates a header that contains the data of the insertion
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+
+    }
+
 
 }
--- src/main/java/com/course/demo/services/UserService.java
+++ src/main/java/com/course/demo/services/UserService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import com.course.demo.domain.User;
+import com.course.demo.dto.UserDTO;
 import com.course.demo.repository.UserRepository;
 import com.course.demo.services.exception.ObjectNotfoundException;
 
@@ -13,16 +14,24 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
 
-    @Autowired
-    private UserRepository repo; // dependecy injection by SpringBoot
+  @Autowired
+  private UserRepository repo; // dependecy injection by SpringBoot
 
-    public List<User> findAll() {
-        return repo.findAll();
-    }
+  public List<User> findAll() {
+    return repo.findAll();
+  }
 
-    public User findById(String id) {
-      Optional<User> obj = repo.findById(id);
-      return obj.orElseThrow(() -> new ObjectNotfoundException("Object not found!"));
-    }
+  public User findById(String id) {
+    Optional<User> obj = repo.findById(id);
+    return obj.orElseThrow(() -> new ObjectNotfoundException("Object not found!"));
+  }
+
+  public User insert(User obj) {
+    return repo.insert(obj);
+  }
+
+  public User fromDTO(UserDTO objDTO) {
+    return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
+  }
 
 }
