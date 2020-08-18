00dcbd4fa1c563e95a6d5124952342613f8f51eb
Andre Faria <andrefaria-1@hotmail.com>
2020-08-18 11:02:03 -0300

    Referencing User's posts

--- src/main/java/com/course/demo/domain/User.java
+++ src/main/java/com/course/demo/domain/User.java
@@ -1,11 +1,14 @@
 package com.course.demo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "user") //indicates that we are working with a MongoDB collection
+@Document(collection = "user") // indicates that we are working with a MongoDB collection
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
@@ -13,6 +16,9 @@ public class User implements Serializable {
     private String id;
     private String name;
     private String email;
+
+    @DBRef (lazy = true) // saves memory: if only User data is needed the posts won't load up automatically. 
+    private List<Post> posts = new ArrayList<>();
 
     public User() {
     }
@@ -27,7 +33,7 @@ public class User implements Serializable {
         return id;
     }
 
-    public void setId(String id){
+    public void setId(String id) {
         this.id = id;
     }
 
@@ -45,6 +51,14 @@ public class User implements Serializable {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
     }
 
     @Override
