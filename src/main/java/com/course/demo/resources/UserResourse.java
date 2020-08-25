75a2e61083e156459f79a7a6a8523e13eca91e98
Andre Faria <andrefaria-1@hotmail.com>
2020-08-24 21:32:11 -0300

    Endpoint to return Users posts

--- src/main/java/com/course/demo/resources/UserResourse.java
+++ src/main/java/com/course/demo/resources/UserResourse.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.course.demo.domain.Post;
 import com.course.demo.domain.User;
 import com.course.demo.dto.UserDTO;
 import com.course.demo.services.UserService;
@@ -63,6 +64,13 @@ public class UserResourse {
         obj.setId(id); // updates de id number to the id given as argument
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
+
     }
 
 }
