5696b8ed97d669f4cd1c4ec879cc632254268b3f
Andre Faria <andrefaria-1@hotmail.com>
2020-08-24 22:10:10 -0300

    Getting Post by Id

---
+++ src/main/java/com/course/demo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.course.demo.services;
+
+import java.util.Optional;
+
+import com.course.demo.domain.Post;
+import com.course.demo.repository.PostRepository;
+import com.course.demo.services.exception.ObjectNotfoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostService {
+
+  @Autowired
+  private PostRepository repo;
+
+  public Post findById(String id) {
+    Optional<Post> user = repo.findById(id);
+    return user.orElseThrow(()-> new ObjectNotfoundException("Object not found!"));
+  }
+}
